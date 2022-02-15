9de2c104c51a73c92d9592ca0e3c041cec7163ee
Alex <iliynsd@icloud.com>
2022-02-15 18:55:33 +0300

    add class InputValidationUtil and methods to check if entered correct data

---
+++ ConsoleApp4/Utils/InputValidationUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp4.Utils
+{
+    public static class InputValidationUtil
+    {
+
+        public static bool LessThanTwoNums(List<string> numbers) => numbers.Count < 3;
+
+        public static bool CheckIsNumbers(List<string> numbers)
+        {
+            foreach (var number in numbers)
+            { 
+                foreach (var sym in number)
+                { 
+                    if (!int.TryParse(sym.ToString(), out int symb))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CorrectEnter(List<string> numbers) => CheckIsNumbers(numbers) && LessThanTwoNums(numbers);
+    }
+}
