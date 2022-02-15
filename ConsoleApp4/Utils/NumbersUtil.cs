b72e31e6376007400f423219737b6db3787a5963
Alex <iliynsd@icloud.com>
2022-02-15 18:56:29 +0300

    add class and method to order nums in list with strings

---
+++ ConsoleApp4/Utils/NumbersUtil.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4.Utils
+{
+    public static class NumbersUtil
+    {
+        public static List<int> OrderNums(List<string> nums) => nums.Select(num => int.Parse(num)).OrderBy(i=>i).ToList();
+    }
+}
